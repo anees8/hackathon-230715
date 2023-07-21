a7e21518d7e026cf1e72950b120865a474572b4d
anees8 <aneesahmed8484@gmail.com>
2023-07-21 19:54:56 +0530

    ViewTailorOrder Page Create

---
+++ src/views/Tailors/ViewTailorOrder.vue
@@ -0,0 +1,121 @@
+<script setup>
+import { storeToRefs } from "pinia";
+import { useRoute } from 'vue-router';
+import { useTailorOrdersStore } from "../../stores/tailororders";
+const {
+  orders,
+  fields,
+  isBusy,
+  errors,
+  options,
+  perPage,
+  currentPage,
+  rows
+} = storeToRefs(useTailorOrdersStore());
+
+const {
+  getOrders,
+  setPerPage,
+  acceptOrder
+} = useTailorOrdersStore();
+
+const route = useRoute();
+const tailorID=parseInt(route.params.id);
+getOrders(tailorID);
+</script>
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-card>
+        <b-col>
+          <b-row align-v="center">
+            <b-col><h5>Orders List - ( TOTAL : {{ rows }})</h5></b-col>
+          </b-row>
+        </b-col>
+        <b-col
+          ><b-table
+            striped
+            outlined
+            empty-filtered-text
+            caption-top
+            hover
+            footClone
+            small
+           :items="orders"
+            :fields="fields"
+            :busy="isBusy"
+            responsive
+            show-empty
+          >
+             <template #cell(product_id)="data">{{
+            data.item.sku.name
+            }} - ({{
+           (data.item.sku.sku_code)
+            }})</template>
+            <template #cell(product_type)="data">{{
+              data.item.sku.product_type.name
+            }}</template>
+            <template #cell(product_size)="data">{{
+              data.item.sku.size.name
+            }}</template>
+            <template #cell(qty)="data">{{
+              data.item.quantity
+            }}</template>
+            <template #cell(commission)="data">{{
+           Math.floor(((data.item.sku.price)*(1/6)),2)
+            }}</template>
+            <template #cell(total_commission)="data">{{
+              data.item.quantity * Math.floor(((data.item.sku.price)*(1/6)),2)
+            }}</template>
+            <template #cell(actions)="data"> 
+              
+              
+              <b-button
+                class="rounded p-2 me-2"
+                size="sm"
+                variant="outline-success"
+                @click="acceptOrder(data.item.id,tailorID,1)"
+
+              >
+                <FontAwesomeIcon icon="check" /> Accept
+              </b-button>
+              <b-button
+                class="rounded p-2 me-2"
+                size="sm"
+                variant="outline-danger"
+                @click="acceptOrder(data.item.id,tailorID,0)" 
+              >
+                <FontAwesomeIcon icon="xmark" /> Reject
+              </b-button>
+              
+              </template>
+        </b-table
+        ></b-col>
+        <b-row align-h="end" class="mt-5">
+          <b-col xl="1" lg="2" md="2" class="p-2">
+            <b-form-select
+              v-if="rows > 5"
+              v-model="perPage"
+              :options="options"
+              size="md"
+              v-on:change="setPerPage"
+              varient="dark"
+            ></b-form-select>
+
+          </b-col>
+
+         
+          <b-col xl="5" lg="6" md="8" class="p-2">
+            <b-pagination
+              v-if="rows / perPage > 1"
+              v-on:click="getOrders"
+              v-model="currentPage"
+              :total-rows="rows"
+              :per-page="perPage"
+            ></b-pagination>
+          </b-col>
+        </b-row>
+      </b-card>
+    </b-row>
+  </b-container>
+</template>
