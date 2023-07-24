6a57b0549522474b6be427c10c89d3cfe46c380f
anees8 <aneesahmed8484@gmail.com>
2023-07-24 17:52:07 +0530

    Tailor added Relationship(tailer_order)

--- app/Models/Tailor.php
+++ app/Models/Tailor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 use App\Models\Order;
+use App\Models\OrderTailor;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,12 +11,22 @@ class Tailor extends Model
     use HasFactory;
     protected $guarded = [];
     
-    public function orders(){
-      return $this->hasMany(Order::class);
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_tailor')
+                    ->withPivot('status')
+                    ->withTimestamps();
     }
 
     public function productTypes()
     {
         return $this->belongsToMany(ProductType::class, 'tailor_product_type', 'tailor_id', 'product_type_id');
     }
+
+    public function tailer_order()
+    {
+        return $this->belongsToMany(OrderTailor::class, 'order_tailors', 'id', 'tailer_id');
+    }
+
 }
